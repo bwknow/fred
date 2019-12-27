5414f2346c264d779b7d5c59825762872cc36cf3
Brian Knowles <bwknow@gmail.com>
2019-12-27 13:29:43 -0800

    Update Resource.js

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -2,11 +2,11 @@ export const resource = {
  
   PB_FORM_PRIVACY_POLICY: 'Privacy Policy',
   PB_FORM_TERMS: 'Terms & Conditions',
-  PB_FORM_COPYRIGHT: '© 2018 Starbucks Corporation. All rights reserved.',
+  PB_FORM_COPYRIGHT: '© 2018  All rights reserved.',
   ERROR_FILETYPE_NOT_SUPPORTED: 'Error! File type not supported. Use plain text files.',
   PB_TAGLINE: 'The ultimate marketing tool',
-  PRIVACY_POLICY_URL: "https://www.starbucks.com/about-us/company-information/online-policies/privacy-policy",
-  TERMS_OF_USE: "https://www.starbucks.com/about-us/company-information/online-policies/terms-of-use",
+  PRIVACY_POLICY_URL: "",
+  TERMS_OF_USE: "",
   FACE_ATTRIBUTES: 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
   
   VISION_KEY: '',
